64dbfd76213955b52c0860e8be068c5d229d2946
Syed-Hamza10 <bsdsf21a019@pucit.edu.pk>
2023-12-31 22:20:21 +0500

    Just Starting out

---
+++ pattern_1.py
@@ -0,0 +1,11 @@
+#****
+#****
+#****
+#****
+
+def pattern(size):
+    for i in range(size):
+        for j in range(size):
+            print('*', end = " ")
+        print()    
+pattern(4)            
