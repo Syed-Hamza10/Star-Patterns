45e6615ba004df9db1dff4a7967938dca0564908
Syed-Hamza10 <bsdsf21a019@pucit.edu.pk>
2024-01-01 23:01:54 +0500

    NY Resolution to code everyday and not forget pushing it to Github

---
+++ patten_2.py
@@ -0,0 +1,20 @@
+# *
+# **
+# ***
+# ****
+# *****
+
+
+#way 1
+def pattern(size):
+    for i in range(size):
+        print('*' * (i+1))
+
+#pattern(5) 
+
+def way2(size):
+    for i in range(size):
+        for j in range(i+1):
+            print('*', end='')
+        print()    
+way2(5)
